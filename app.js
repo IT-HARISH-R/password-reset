faccb7a2a35086f2802fadd6451e338a6b608fb6
IT-HARISH-R <harishmass27.8@gmail.com>
2025-01-05 14:43:39 +0530

    remove cord

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser')
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); 
 // app.use(cors({
 //     origin: ['https://task12-password-reset.netlify.app'],
 //     credentials: true
