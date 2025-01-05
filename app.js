54d3ee25d5f9698abf699366c417980b128a77e9
IT-HARISH-R <harishmass27.8@gmail.com>
2025-01-05 14:52:47 +0530

    cors add

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const express = require("express");
 const logger = require("./utlis/logger");
 const authRouter = require("./routers/authRout");
 const errorPage = require("./utlis/errorPage");
-// const cors = require("cors");
+const cors = require("cors");
 const cookieParser = require('cookie-parser')
 
 const app = express();
 
 app.use(express.json()); 
-// app.use(cors({
-//     origin: ['https://task12-password-reset.netlify.app'],
-//     credentials: true
-// }))
+app.use(cors({
+    origin: ['https://task12-password-reset.netlify.app'],
+    credentials: true
+}))
 app.use(cookieParser())
 app.use(logger)
 app.use("/auth", authRouter);
