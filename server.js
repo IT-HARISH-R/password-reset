d79d5e74422ada6caab1641b43fa7d1a52342d8a
IT-HARISH-R <harishmass27.8@gmail.com>
2025-01-05 14:22:01 +0530

    add

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ mongoose.connect(MONGODB_URL)
     .then(() => {
         console.log("Connecting database")
         app.listen(PORT, () => {
-            console.log("server run at http://127.0.0.1:3000");
+            console.log("server run");
         })
     })
     .catch((error) => {
